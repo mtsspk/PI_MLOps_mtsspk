ec723f618bb821b277ee6c07d7bbc2b3cc54c371
mtsspk <138814069+mtsspk@users.noreply.github.com>
2024-01-10 05:38:41 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,47 @@
+import pandas as pd
+from fastapi import FastAPI, HTTPException
+import pyarrow.parquet as pq
+import api_functions
+import numpy as np
+
+app = FastAPI()
+
+
+@app.get("/")
+def index():
+    return {"Proyecto Steam"}
+
+
+@app.get("/PlayTimeGenre")
+def PlayTimeGenre(genre: str):
+    return api_functions.PlayTimeGenre(genre)    
+
+
+@app.get("/UserForGenre")
+def UserForGenre(genre: str):
+    return api_functions.UserForGenre(genre) 
+
+
+@app.get("/UsersRecommend")
+def UsersRecommend(posted_year: int):
+    return api_functions.UsersRecommend(posted_year)
+
+
+@app.get("/UsersWorstDeveloper")
+def UsersWorstDeveloper(posted_year: int):
+    return api_functions.UsersWorstDeveloper(posted_year) 
+    
+
+@app.get("/sentiment_analysis/{developer}")
+def sentiment_analysis(developer: str):
+    return api_functions.sentiment_analysis(developer) 
+
+
+@app.get("/recomendacion_juego/{game_id}")
+def recomendacion_juego(game_id: str, num_recommendations: int = 5):
+    return api_functions.recomendacion_juego(game_id, num_recommendations) 
+
+
+@app.get("/recomendacion_usuario/{user_id}")
+def recomendacion_usuario(user_id: str, num_recommendations: int = 5):
+    return api_functions.recomendacion_usuario(user_id) 
